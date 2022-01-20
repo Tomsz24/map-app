d2dd642fdc9b1c7094cb27b3f4c60208a11c3bef
Tomasz <tomaszwojciechowski244@gmail>
2022-01-20 23:22:14 +0100

    create markers on coordinates provided by api for all vehicles, install axios and fontawsome in dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import GoogleMapReact from 'google-map-react';
+import axios from "axios";
+import Marker from "./components/Marker/Marker";
+
 
 function App() {
+  const mapRef = useRef();
+  const [zoom, setZoom] = useState(10);
+  const [bounds, setBounds] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect( async () => {
+    const result =  await axios.get('https://dev.vozilla.pl/api-client-portal/map?objectType=VEHICLE');
+    setData(result.data.objects)
+  },[]);
+
+
   return (
     <div style={{height: '100vh', width: '100%'}}>
       <GoogleMapReact
@@ -9,6 +23,12 @@ function App() {
         defaultCenter={{lat: 52.1935161702226, lng: 20.9304286193486}}
         defaultZoom={10}
       >
+        {data.map(item => {
+          console.log(item);
+          return (
+            <Marker key={item.platesNumber} lat={item.location.latitude} lng={item.location.longitude}/>
+          )
+        })}
       </GoogleMapReact>
     </div>
   );
