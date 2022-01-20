9db9664fae0b7890b63a8da92ca5d4a59ae93752
Tomasz <tomaszwojciechowski244@gmail>
2022-01-20 21:37:52 +0100

    configure google map component, install styled components and do little cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleMapReact from 'google-map-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '100vh', width: '100%'}}>
+      <GoogleMapReact
+        bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_KEY}}
+        defaultCenter={{lat: 52.1935161702226, lng: 20.9304286193486}}
+        defaultZoom={10}
+      >
+      </GoogleMapReact>
     </div>
   );
 }
